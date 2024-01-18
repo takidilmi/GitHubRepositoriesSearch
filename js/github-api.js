190122e1433227d2235eb008e40df76f58dfb000
takidilmi <angeldilmi@gmail.com>
2024-01-18 18:31:44 +0100

    add each repositories card styling

--- js/github-api.js
+++ js/github-api.js
@@ -25,5 +25,12 @@ async function getRepositories(user, callback) {
   const reposResponse = await fetch(`${GITHUB_API_URL}/users/${user}/repos`);
   const reposData = await reposResponse.json();
 
+  for (let i = 0; i < reposData.length; i++) {
+    const repo = reposData[i];
+    const languagesResponse = await fetch(repo.languages_url);
+    const languagesData = await languagesResponse.json();
+    repo.languages = Object.keys(languagesData);
+  }
+
   callback(data, reposData);
 }
--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
-// script.js
-document.getElementById('search-form').addEventListener('submit', function(event) {
+document
+  .getElementById("search-form")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    const username = document.getElementById('username').value;
-    getRepositories(username, function(user, repos) {
-        const userDiv = document.getElementById('user');
-        userDiv.innerHTML = `
-            <div class="d-flex w-75 align-items-center justify-content-around"><img class="img-thumbnail rounded-4 w-50" src="${user.avatar_url}" alt="${user.name}'s avatar">
-            <h2>${user.name}</h2></div>
+    const username = document.getElementById("username").value;
+    getRepositories(username, function (user, repos) {
+      const userDiv = document.getElementById("user");
+      userDiv.innerHTML = `
+            <div class="d-flex w-75 align-items-center justify-content-around">
+            <img class="img-thumbnail rounded-4 w-50" src="${user.avatar_url}" alt="${user.name}'s avatar">
+            <h2>${user.name}</h2>
+            </div>
         `;
 
-        const reposDiv = document.getElementById('repos');
-        reposDiv.innerHTML = '';
-        repos.forEach(function(repo) {
-            const div = document.createElement('div');
-            div.className = 'col-sm-6';
-            div.textContent = repo.name;
-            reposDiv.appendChild(div);
-        });
+      const reposDiv = document.getElementById("repos");
+      reposDiv.innerHTML = "";
+      repos.forEach(function (repo) {
+        const div = document.createElement("div");
+        div.className = "col-sm-6";
+        div.innerHTML = `
+          <div class="card p-2 h-100">
+              <h5 class="card-title">${repo.name}</h5>
+              <p class="card-text">${repo.description}</p>
+              <p class="card-text">
+            ${repo.languages
+              .map(
+                (language) => `<span class="btn btn-primary">${language}</span>`
+              )
+              .join(" ")}
+          </p>
+          </div>
+        `;
+        reposDiv.appendChild(div);
+      });
     });
-});
+  });
