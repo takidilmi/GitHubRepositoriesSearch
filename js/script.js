0ee1d6a89e5e4751770bd4b125dffffa2d49ad8c
takidilmi <angeldilmi@gmail.com>
2024-01-18 16:35:52 +0100

    add styling to the search and repositories

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
+// script.js
 document.getElementById('search-form').addEventListener('submit', function(event) {
     event.preventDefault();
     const username = document.getElementById('username').value;
     getRepositories(username, function(user, repos) {
         const userDiv = document.getElementById('user');
         userDiv.innerHTML = `
-            <img src="${user.avatar_url}" alt="${user.name}'s avatar">
-            <h2>${user.name}</h2>
+            <div class="d-flex w-75 align-items-center justify-content-around"><img class="img-thumbnail rounded-4 w-50" src="${user.avatar_url}" alt="${user.name}'s avatar">
+            <h2>${user.name}</h2></div>
         `;
 
         const reposDiv = document.getElementById('repos');
         reposDiv.innerHTML = '';
         repos.forEach(function(repo) {
-            const p = document.createElement('p');
-            p.textContent = repo.name;
-            reposDiv.appendChild(p);
+            const div = document.createElement('div');
+            div.className = 'col-sm-6';
+            div.textContent = repo.name;
+            reposDiv.appendChild(div);
         });
     });
 });
